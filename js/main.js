a44810497a6259f8dbf680d3907adf0201d61f99
Serhan Ramadan <you@example.comsr-webdesigns@outlook.com>
2020-11-07 03:08:08 +0000

    Updates

---
+++ js/main.js
@@ -0,0 +1,15 @@
+//*? INFO SECTION */
+(() => {
+  const aboutSection = document.querySelector('.section-about');
+  tabsContainer = document.querySelector('.about-tabs');
+
+  tabsContainer.addEventListener('click', (event) => {
+    if (event.target.classList.contains('tab-item') && !event.target.classList.contains('active')) {
+      const target = event.target.getAttribute('data-target');
+      tabsContainer.querySelector('.active').classList.remove('outer-shadow', 'active');
+      event.target.classList.add('active', 'outer-shadow');
+      aboutSection.querySelector('.tab-content.active').classList.remove('active');
+      aboutSection.querySelector(target).classList.add('active');
+    }
+  });
+})();
