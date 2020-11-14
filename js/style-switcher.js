dbc65078a3e4b5f73a6a543f02aea7aac29481c8
Serhan Ramadan <you@example.comsr-webdesigns@outlook.com>
2020-11-14 07:09:06 +0000

    Updates

---
+++ js/style-switcher.js
@@ -0,0 +1,47 @@
+//*? **************************************** STYLE SWITCHER  TOGGLE **************************************** */
+
+// const styleSwitcherToggler = document.querySelector('.style-switcher-toggler');
+
+// styleSwitcherToggler.addEventListener('click', () => {
+//   document.querySelector('.style-switcher').classList.toggle('open');
+// });
+
+//*? **************************************** HIDE ON SCROLL STYLE SWITCHER  TOGGLE **************************************** */
+
+// window.addEventListener('scroll', () => {
+//   if (document.querySelector('.style-switcher').classList.contains('open')) {
+//     document.querySelector('.style-switcher').classList.remove('open');
+//   }
+// });
+
+// //*? **************************************** THEME COLORS **************************************** */
+
+// const alternateStyles = document.querySelectorAll('.alternate-style');
+
+// function setActiveStyle(color) {
+//   alternateStyles.forEach((style) => {
+//     if (color === style.getAttribute('title')) {
+//       style.removeAttribute('disabled');
+//     } else {
+//       style.setAttribute('disabled', 'true');
+//     }
+//   });
+// }
+
+// //*? **************************************** DARK MODE **************************************** */
+
+const dayNight = document.querySelector('.day-night');
+
+dayNight.addEventListener('click', () => {
+  dayNight.querySelector('i').classList.toggle('fa-sun');
+  dayNight.querySelector('i').classList.toggle('fa-moon');
+  document.body.classList.toggle('dark');
+});
+
+window.addEventListener('load', () => {
+  if (document.body.classList.contains('dark')) {
+    dayNight.querySelector('i').classList.add('fa-sun');
+  } else {
+    dayNight.querySelector('i').classList.add('fa-moon');
+  }
+});
